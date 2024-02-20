66efcac3e0a2f8ce26e23e24e3f1db30e69543da
Boitumelo Middas  Mokobane <125658597+middas123@users.noreply.github.com>
2024-02-20 13:56:31 +0200

    sdfhhds

--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get( '/testServer', (req, res)=> {
+    res.statusCode(200);
+});
+
+app.get('/', (res, req)=> {
+    res.statusCode(200);
+});
+
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT )
-})
+});
